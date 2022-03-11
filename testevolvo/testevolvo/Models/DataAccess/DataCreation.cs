3b9e002c3c59d9bd5abf5a2bbf88a83d5e9340d6
Henry Turner Lizidatti Rosolini <8486186+htlrosolini@users.noreply.github.com>
2022-03-11 00:33:08 -0300

    Criação de Models para Banco de dados e Controllers de API

---
+++ testevolvo/testevolvo/Models/DataAccess/DataCreation.cs
@@ -0,0 +1,21 @@
+﻿using testevolvo.Models.Entities.Volvo;
+
+namespace testevolvo.Models.DataAccess
+{
+    public class DataCreation
+    {
+        public static void CreateTables()
+        {
+            CaminhaoAccess.CreateTableIfNotExists();
+        }
+
+        public static void CreateDataSample()
+        {
+            if (CaminhaoAccess.Count() <= 0)
+            {
+                CaminhaoAccess.Save(new Caminhao { Modelo = "FH", Name = "Caminhão 1", AnoFab = 2021, AnoMod = 2022 });
+                CaminhaoAccess.Save(new Caminhao { Modelo = "FM", Name = "Caminhão 2", AnoFab = 2021, AnoMod = 2022 });
+            }
+        }
+    }
+}
---
+++ testevolvo/testevolvo/Models/Entities/Volvo/Caminhao.cs
@@ -0,0 +1,18 @@
+﻿using SQLiteORM.Attributes;
+using System.Text.Json.Serialization;
+
+namespace testevolvo.Models.Entities.Volvo
+{
+    [Table("Caminhoes")]
+    public class Caminhao
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Modelo { get; set; }
+        public int AnoFab { get; set; }
+        public int AnoMod { get; set; }
+    }
+}
